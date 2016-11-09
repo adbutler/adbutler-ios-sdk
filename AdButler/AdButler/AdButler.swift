80dcc9fefb383db072001cd0190af3ef8e620705
Ryuichi Saito <ryuichi@ryuichisaito.com>
2016-11-08 20:43:21 -0600

    The happy path quick implementation for requesting a placement

---
+++ AdButler/AdButler/AdButler.swift
@@ -0,0 +1,39 @@
+//
+//  AdButler.swift
+//  AdButler
+//
+//  Created by Ryuichi Saito on 11/8/16.
+//  Copyright © 2016 AdButler. All rights reserved.
+//
+
+import Foundation
+
+public class AdButler {
+    public func requestPlacement(with config: PlacementRequestConfig, completionHandler: () -> Void) {
+        let urlString = "https://servedbyadbutler.com/adserve/\(config.queryString);type=json"
+        guard let url = URL(string: urlString) else {
+            completionHandler() // TODO: error handling
+            return
+        }
+        let request = URLRequest(url: url)
+        
+        let sessionConfig = URLSessionConfiguration.ephemeral
+        sessionConfig.httpAdditionalHeaders = [
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        ]
+        let session = URLSession(configuration: sessionConfig)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            if let _ = error {
+                completionHandler() // TODO: error handling
+            } else if let httpResponse = response as? HTTPURLResponse, let data = data, httpResponse.statusCode == 200 {
+                if let json = JSONSerialization.jsonObject(with: data) {
+                    print(json)
+                }
+            } else {
+                completionHandler() // TODO: error handling
+            }
+        }
+        task.resume()
+    }
+}
