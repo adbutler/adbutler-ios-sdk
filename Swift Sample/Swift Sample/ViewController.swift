ea5618ad6e39c94317a0afb4a52fd5474db703b7
Ryuichi Saito <ryuichi@ryuichisaito.com>
2016-12-03 09:38:53 -0600

    Make Swift sample easier to understand

--- Swift Sample/Swift Sample/ViewController.swift
+++ Swift Sample/Swift Sample/ViewController.swift
@@ -12,7 +12,20 @@ import AdButler
 class ViewController: UIViewController {
     @IBAction func requestPlacementTapped(_ sender: Any) {
         let config = PlacementRequestConfig(accountId: 153105, zoneId: 214764, width: 300, height: 250)
-        AdButler.requestPlacement(with: config, completionHandler: placementResponseHandler)
+        AdButler.requestPlacement(with: config) { response in
+            switch response {
+            case .success(let status, let placements):
+                print(status.rawValue)
+                print(placements.map({ $0.debugString }))
+            case .badRequest(let statusCode, let responseBody):
+                print(statusCode ?? -1)
+                print(responseBody ?? "<no body>")
+            case .invalidJson(let responseBody):
+                print(responseBody ?? "<no body>")
+            case .requestError(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
     
     @IBAction func requestPlacementsTapped(_ sender: Any) {
@@ -20,21 +33,19 @@ class ViewController: UIViewController {
             PlacementRequestConfig(accountId: 153105, zoneId: 214764, width: 300, height: 250),
             PlacementRequestConfig(accountId: 153105, zoneId: 214764, width: 300, height: 250, keywords: ["sample2"]),
         ]
-        AdButler.requestPlacements(with: configs, completionHandler: placementResponseHandler)
-    }
-    
-    private func placementResponseHandler(response: Response) {
-        switch response {
-        case .success(let status, let placements):
-            print(status.rawValue)
-            print(placements.map({ $0.debugString }))
-        case .badRequest(let statusCode, let responseBody):
-            print(statusCode ?? -1)
-            print(responseBody ?? "<no body>")
-        case .invalidJson(let responseBody):
-            print(responseBody ?? "<no body>")
-        case .requestError(let error):
-            print(error.localizedDescription)
+        AdButler.requestPlacements(with: configs) { response in
+            switch response {
+            case .success(let status, let placements):
+                print(status.rawValue)
+                print(placements.map({ $0.debugString }))
+            case .badRequest(let statusCode, let responseBody):
+                print(statusCode ?? -1)
+                print(responseBody ?? "<no body>")
+            case .invalidJson(let responseBody):
+                print(responseBody ?? "<no body>")
+            case .requestError(let error):
+                print(error.localizedDescription)
+            }
         }
     }
     
@@ -47,22 +58,26 @@ class ViewController: UIViewController {
     }
     
     @IBAction func recordImpressionTapped(_ sender: Any) {
-        recordPlacement(recordClosure: { $0.recordImpression() })
+        let placement = getSamplePlacement()
+        placement.recordImpression()
     }
     
     @IBAction func recordClickTapped(_ sender: Any) {
-        recordPlacement(recordClosure: { $0.recordClick() })
+        let placement = getSamplePlacement()
+        placement.recordClick()
     }
     
-    private func recordPlacement(recordClosure: @escaping (Placement) -> Void) {
-        let config = PlacementRequestConfig(accountId: 153105, zoneId: 214764, width: 300, height: 250, keywords: ["sample2"])
-        AdButler.requestPlacement(with: config, completionHandler: { response in
-            guard case let .success(status, placements) = response, status == .success else {
-                return
-            }
-            
-            placements.forEach(recordClosure)
-        })
+    private func getSamplePlacement() -> Placement {
+        return Placement(
+            bannerId: 519407754,
+            redirectUrl: "https://servedbyadbutler.com/redirect.spark?MID=153105&plid=550986&setID=214764&channelID=0&CID=0&banID=519407754&PID=0&textadID=0&tc=1&mt=1480778998606477&hc=534448fb7fb5835eaca37f949e61a363d8237324&location=",
+            imageUrl: "http://servedbyadbutler.com/default_banner.gif",
+            width: 300,
+            height: 250,
+            altText: "",
+            target: "_blank",
+            trackingPixel: "http://servedbyadbutler.com/default_banner.gif?foo=bar&demo=fakepixel",
+            accupixelUrl: "https://servedbyadbutler.com/adserve.ibs/;ID=153105;size=1x1;type=pixel;setID=214764;plid=550986;BID=519407754;wt=1480779008;rnd=90858")
     }
 }
 
