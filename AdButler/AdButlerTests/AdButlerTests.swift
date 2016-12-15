c3cf39168fb871438ce83c8ebada6df3769261e3
Ryuichi Saito <ryuichi@ryuichisaito.com>
2016-12-14 21:03:43 -0600

    Add unit test target

---
+++ AdButler/AdButlerTests/AdButlerTests.swift
@@ -0,0 +1,13 @@
+//
+//  AdButlerTests.swift
+//  AdButlerTests
+//
+//  Created by Ryuichi Saito on 12/14/16.
+//  Copyright © 2016 AdButler. All rights reserved.
+//
+
+import XCTest
+
+class AdButlerTests: XCTestCase {
+    
+}
