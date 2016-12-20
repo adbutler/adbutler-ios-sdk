ac0d681aab4b1a7b358a7237e32713bad2527a94
Ryuichi Saito <ryuichi@ryuichisaito.com>
2016-12-19 20:42:03 -0600

    Tests for making placement requests

--- AdButler/AdButlerTests/AdButlerTests.swift
+++ AdButler/AdButlerTests/AdButlerTests.swift
@@ -11,8 +11,37 @@ import XCTest
 @testable import AdButler
 
 class AdButlerTests: XCTestCase {
-    func testRequest() {
-        AdButler.session = TestSession.sample1.session
+    private let placement1 = [
+        "banner_id": "1",
+        "redirect_url": "https://servedbyadbutler.com/redirect.spark?MID=153105&plid=543820&setID=214764&channelID=0&CID=0&banID=519401954&PID=0&textadID=0&tc=1&mt=1482121253469173&hc=fdc53f2a708e63ffd71bf224471433202cd31416&location=",
+        "image_url": "https://servedbyadbutler.com/default_banner.gif",
+        "width": "300",
+        "height": "250",
+        "alt_text": "",
+        "target": "_blank",
+        "tracking_pixel": "",
+        "accupixel_url": "https://servedbyadbutler.com/adserve.ibs/;ID=153105;size=1x1;type=pixel;setID=214764;plid=543820;BID=519401954;wt=1482121263;rnd=99929",
+        "refresh_url": "",
+        "refresh_time": "",
+        "body": ""
+    ]
+    
+    private let placement2 = [
+        "banner_id": "2",
+        "redirect_url": "https://servedbyadbutler.com/redirect.spark?MID=153105&plid=550986&setID=214764&channelID=0&CID=0&banID=519407754&PID=0&textadID=0&tc=1&mt=1482121259961736&hc=81916f6603f91c20aecb6693d4a38ad9f632db6f&location=",
+        "image_url": "https://servedbyadbutler.com/default_banner.gif",
+        "width": "300",
+        "height": "250",
+        "alt_text": "",
+        "target": "_blank",
+        "tracking_pixel": "https://servedbyadbutler.com/default_banner.gif?foo=bar&demo=fakepixel",
+        "accupixel_url": "https://servedbyadbutler.com/adserve.ibs/;ID=153105;size=1x1;type=pixel;setID=214764;plid=550986;BID=519407754;wt=1482121269;rnd=44297",
+        "refresh_url": "",
+        "refresh_time": "",
+        "body": ""]
+    
+    func testRequestPlacement() {
+        AdButler.session = TestSession.success(placement1).session
         let config = PlacementRequestConfig(accountId: 153105, zoneId: 214764, width: 300, height: 250)
         let expct = expectation(description: "expect to get a response")
         AdButler.requestPlacement(with: config) { (response) in
@@ -30,7 +59,83 @@ class AdButlerTests: XCTestCase {
             }
             
             let placement = placements[0]
-            XCTAssertEqual(placement.bannerId, 519401954)
+            XCTAssertEqual(placement.bannerId, 1)
+        }
+        waitForExpectations(timeout: 3)
+    }
+    
+    func testRequestMultiplePlacements() {
+        AdButler.session = TestSession.success(placement2).session
+        let config = PlacementRequestConfig(accountId: 153105, zoneId: 214764, width: 300, height: 250)
+        let expct = expectation(description: "expect to get a response")
+        AdButler.requestPlacements(with: [config, config]) { (response) in
+            expct.fulfill()
+            
+            guard case let .success(status, placements) = response else {
+                XCTFail()
+                return
+            }
+            
+            XCTAssertEqual(status.rawValue, "SUCCESS")
+            guard placements.count == 2 else {
+                XCTFail()
+                return
+            }
+            
+            XCTAssertEqual(placements[0].bannerId, 2)
+            XCTAssertEqual(placements[1].bannerId, 2)
+        }
+        waitForExpectations(timeout: 3)
+    }
+    
+    func testBadRequest() {
+        AdButler.session = TestSession.badRequest(400).session
+        let config = PlacementRequestConfig(accountId: 153105, zoneId: 214764, width: 300, height: 250)
+        let expct = expectation(description: "expect to get a response")
+        AdButler.requestPlacement(with: config) { (response) in
+            expct.fulfill()
+            
+            guard case let .badRequest(statusCode?, body?) = response else {
+                XCTFail()
+                return
+            }
+            
+            XCTAssertEqual(statusCode, 400)
+            XCTAssertEqual(body, "bad request")
+        }
+        waitForExpectations(timeout: 3)
+    }
+    
+    func testInvalidJSON() {
+        AdButler.session = TestSession.invalidJson.session
+        let config = PlacementRequestConfig(accountId: 153105, zoneId: 214764, width: 300, height: 250)
+        let expct = expectation(description: "expect to get a response")
+        AdButler.requestPlacement(with: config) { (response) in
+            expct.fulfill()
+            
+            guard case let .invalidJson(body?) = response else {
+                XCTFail()
+                return
+            }
+            
+            XCTAssertEqual(body, "invalid json")
+        }
+        waitForExpectations(timeout: 3)
+    }
+    
+    func testRequestError() {
+        AdButler.session = TestSession.requestError.session
+        let config = PlacementRequestConfig(accountId: 153105, zoneId: 214764, width: 300, height: 250)
+        let expct = expectation(description: "expect to get a response")
+        AdButler.requestPlacement(with: config) { (response) in
+            expct.fulfill()
+            
+            guard case let .requestError(error) = response else {
+                XCTFail()
+                return
+            }
+            
+            XCTAssertTrue(error is TestError)
         }
         waitForExpectations(timeout: 3)
     }
--- AdButler/AdButlerTests/TestSession.swift
+++ AdButler/AdButlerTests/TestSession.swift
@@ -8,35 +8,117 @@
 
 import Foundation
 
+struct TestError : Error {
+    
+}
+
 enum TestSession {
-    case sample1
-    case sample2
+    case success([String: Any])
+    case badRequest(Int)
+    case invalidJson
+    case requestError
 }
 
 extension TestSession {
     var session: URLSession {
         switch self {
-        case .sample1:
-            return Sample1Session()
-        default:
-            return URLSession.shared
+        case .success(let placement):
+            return SuccessSession(placement: placement)
+        case .badRequest(let statusCode):
+            return BadRequestSession(statusCode: statusCode)
+        case .invalidJson:
+            return InvalidJSONSession()
+        case .requestError:
+            return RequestErrorSession()
         }
-    }
-}
-
-class Sample1Session : URLSession {
-    override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
-        return Sample1DataTask(url: url, completion: completionHandler)
-    }
-    
-    override func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
-        return Sample1DataTask(url: request.url!, completion: completionHandler)
     }
 }
 
 typealias TaskCompletion = (Data?, URLResponse?, Error?) -> Void
 
-class Sample1DataTask : URLSessionDataTask {
+class SuccessSession : URLSession {
+    private let _placement: [String: Any]
+    
+    init(placement: [String: Any]) {
+        _placement = placement
+    }
+    
+    override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        return SuccessSessionDataTask(url: url, placement: _placement, completion: completionHandler)
+    }
+    
+    override func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        return SuccessSessionDataTask(url: request.url!, placement: _placement, completion: completionHandler)
+    }
+}
+
+class SuccessSessionDataTask : URLSessionDataTask {
+    private let _url: URL
+    private let _placement: [String: Any]
+    private let _completion: TaskCompletion
+    
+    init(url: URL, placement: [String: Any], completion: @escaping TaskCompletion) {
+        _url = url
+        _placement = placement
+        _completion = completion
+    }
+    
+    override func resume() {
+        let dict: [String: Any] = [
+            "status": "SUCCESS",
+            "placements": ["placement_1": _placement]
+        ]
+        let json = try? JSONSerialization.data(withJSONObject: dict)
+        let response = HTTPURLResponse(url: _url, statusCode: 200, httpVersion: nil, headerFields: nil)
+        _completion(json, response, nil)
+    }
+}
+
+class BadRequestSession : URLSession {
+    private let _statusCode: Int
+    
+    init(statusCode: Int) {
+        _statusCode = statusCode
+    }
+    
+    override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        return BadRequestSessionDataTask(url: url, statusCode: _statusCode, completion: completionHandler)
+    }
+    
+    override func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        return BadRequestSessionDataTask(url: request.url!, statusCode: _statusCode, completion: completionHandler)
+    }
+}
+
+class BadRequestSessionDataTask : URLSessionDataTask {
+    private let _url: URL
+    private let _statusCode: Int
+    private let _completion: TaskCompletion
+    
+    init(url: URL, statusCode: Int, completion: @escaping TaskCompletion) {
+        _url = url
+        _statusCode = statusCode
+        _completion = completion
+    }
+    
+    override func resume() {
+        let badRequestData = "bad request".data(using: .utf8)
+        let response = HTTPURLResponse(url: _url, statusCode: _statusCode, httpVersion: nil, headerFields: nil)
+        _completion(badRequestData, response, nil)
+    }
+}
+
+class InvalidJSONSession : URLSession {
+    override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        return InvalidJSONSessionDataTask(url: url, completion: completionHandler)
+    }
+    
+    override func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        return InvalidJSONSessionDataTask(url: request.url!, completion: completionHandler)
+    }
+}
+
+class InvalidJSONSessionDataTask : URLSessionDataTask {
     private let _url: URL
     private let _completion: TaskCompletion
     
@@ -46,27 +128,33 @@ class Sample1DataTask : URLSessionDataTask {
     }
     
     override func resume() {
-        let dict: [String: Any] = [
-            "status": "SUCCESS",
-            "placements": [
-                "placement_1": [
-                    "banner_id": "519401954",
-                    "redirect_url": "https://servedbyadbutler.com/redirect.spark?MID=153105&plid=543820&setID=214764&channelID=0&CID=0&banID=519401954&PID=0&textadID=0&tc=1&mt=1482121253469173&hc=fdc53f2a708e63ffd71bf224471433202cd31416&location=",
-                    "image_url": "https://servedbyadbutler.com/default_banner.gif",
-                    "width": "300",
-                    "height": "250",
-                    "alt_text": "",
-                    "target": "_blank",
-                    "tracking_pixel": "",
-                    "accupixel_url": "https://servedbyadbutler.com/adserve.ibs/;ID=153105;size=1x1;type=pixel;setID=214764;plid=543820;BID=519401954;wt=1482121263;rnd=99929",
-                    "refresh_url": "",
-                    "refresh_time": "",
-                    "body": ""
-                ]
-            ]
-        ]
-        let json = try? JSONSerialization.data(withJSONObject: dict)
+        let invalidJSONData = "invalid json".data(using: .utf8)
         let response = HTTPURLResponse(url: _url, statusCode: 200, httpVersion: nil, headerFields: nil)
-        _completion(json, response, nil)
+        _completion(invalidJSONData, response, nil)
+    }
+}
+
+class RequestErrorSession : URLSession {
+    override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        return RequestErrorSessionDataTask(url: url, completion: completionHandler)
+    }
+    
+    override func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        return RequestErrorSessionDataTask(url: request.url!, completion: completionHandler)
+    }
+}
+
+class RequestErrorSessionDataTask : URLSessionDataTask {
+    private let _url: URL
+    private let _completion: TaskCompletion
+    
+    init(url: URL, completion: @escaping TaskCompletion) {
+        _url = url
+        _completion = completion
+    }
+    
+    override func resume() {
+        let response = HTTPURLResponse(url: _url, statusCode: 500, httpVersion: nil, headerFields: nil)
+        _completion(nil, response, TestError())
     }
 }
